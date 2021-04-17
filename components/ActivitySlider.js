b78dc4c48d315f75ae556d3e1aad10c28f324a71
ajoncode <ajoncode@gmail.com>
2021-04-17 21:34:18 +0530

    AddEntry ActivitySlider

--- components/ActivitySlider.js
+++ components/ActivitySlider.js
@@ -1,11 +1,21 @@
-import { Text, View } from "react-native";
+import { Slider, Text, View } from "react-native";
 
 import React from "react";
 
-export default function ActivitySlider() {
+export default function ActivitySlider({ max, unit, step, value, onChange }) {
   return (
     <View>
-      <Text>Activity Slider</Text>
+      <Slider
+        step={step}
+        value={value}
+        maximumValue={max}
+        minimumValue={0}
+        onValueChange={onChange}
+      />
+      <View>
+        <Text>{value}</Text>
+        <Text>{unit}</Text>
+      </View>
     </View>
   );
 }
