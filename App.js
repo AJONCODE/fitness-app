31406d3d9f9493c7868339880c145a8803863025
ajoncode <ajoncode@gmail.com>
2021-04-19 16:28:08 +0530

    Creating a Store

--- App.js
+++ App.js
@@ -5,17 +5,22 @@ import { Text, View } from "react-native";
 import ActivitySlider from "./components/ActivitySlider";
 import ActivityStepper from "./components/ActivityStepper";
 import AddEntry from "./components/AddEntry";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducer from "./reducers/index";
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <AddEntry />
-    </View>
+    <Provider store={createStore(reducer)}>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <AddEntry />
+      </View>
+    </Provider>
   );
 }
