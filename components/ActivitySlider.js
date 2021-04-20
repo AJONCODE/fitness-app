a0e840dbb80d272c3da842e1738180387fea5c36
ajoncode <ajoncode@gmail.com>
2021-04-20 19:10:57 +0530

    Style Slider DateHeader and Reset

--- components/ActivitySlider.js
+++ components/ActivitySlider.js
@@ -1,21 +1,36 @@
-import { Slider, Text, View } from "react-native";
+import { Slider, StyleSheet, Text, View } from "react-native";
 
 import React from "react";
+import { gray } from "../utils/colors";
 
 export default function ActivitySlider({ max, unit, step, value, onChange }) {
   return (
-    <View>
+    <View style={styles.row}>
       <Slider
+        style={{ flex: 1 }}
         step={step}
         value={value}
         maximumValue={max}
         minimumValue={0}
         onValueChange={onChange}
       />
-      <View>
-        <Text>{value}</Text>
-        <Text>{unit}</Text>
+      <View style={styles.metricCounter}>
+        <Text style={{ fontSize: 24, textAlign: "center" }}>{value}</Text>
+        <Text style={{ fontSize: 18, color: gray }}>{unit}</Text>
       </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: "row",
+    flex: 1,
+    alignItems: "center",
+  },
+  metricCounter: {
+    width: 85,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
--- components/TextButton.js
+++ components/TextButton.js
@@ -1,11 +1,19 @@
-import { Text, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
 
 import React from "react";
+import { purple } from "../utils/colors";
 
-export default function TextButton({ children, onPress }) {
+export default function TextButton({ children, onPress, style = {} }) {
   return (
     <TouchableOpacity onPress={onPress}>
-      <Text>{children}</Text>
+      <Text style={[styles.reset, style]}>{children}</Text>
     </TouchableOpacity>
   );
 }
+
+const styles = StyleSheet.create({
+  reset: {
+    textAlign: "center",
+    color: purple,
+  },
+});
