a0326c499be6b7781a42d7926691e03d9dc93c6d
ajoncode <ajoncode@gmail.com>
2021-05-16 21:15:36 +0530

    Compass Screen: Undetermined Geo Location

--- components/Compass.js
+++ components/Compass.js
@@ -1,13 +1,24 @@
-import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+import {
+  ActivityIndicator,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { lightPurp, paleRed, white } from "../utils/colors";
 
+import { Foundation } from "@expo/vector-icons";
 import React from "react";
 
 export default function Compass() {
   const [compassState, setCompassState] = React.useState({
     coordinate: null,
-    status: null,
+    // status: null,
+    status: "undetermined",
     direction: "",
   });
+
+  const askPermission = () => {};
 
   if (!compassState.status) {
     return <ActivityIndicator style={{ marginTop: 30 }} />;
@@ -23,8 +34,12 @@ export default function Compass() {
 
   if (compassState.status === "undetermined") {
     return (
-      <View>
-        <Text>Undetermined</Text>
+      <View style={styles.center}>
+        <Foundation name="alert" size={50} color={paleRed} />
+        <Text>Please enable location services for this app.</Text>
+        <TouchableOpacity style={styles.button} onPress={askPermission}>
+          <Text style={styles.buttonText}>ENABLE</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -36,3 +51,28 @@ export default function Compass() {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+  },
+  center: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 30,
+    marginRight: 30,
+  },
+  button: {
+    padding: 10,
+    backgroundColor: lightPurp,
+    alignSelf: "center",
+    borderRadius: 5,
+    margin: 20,
+  },
+  buttonText: {
+    color: white,
+    fontSize: 15,
+  },
+});
